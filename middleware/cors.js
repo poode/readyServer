a14eea7cb62040af6243ba6157ab13fd0cad7ce2
poode <eng.abdul.azeem@gmail.com>
2019-07-07 16:17:16 +0200

    update cors to allow sockets

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,9 +1,10 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: `http://localhost:${process.env.PORT}`,
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  // credentials: true,
+  allowedHeaders: ['x-auth-token', 'Content-Type', 'Authorization', 'Access-Control-Allow-Credentials', 'Access-Control-Request-Method', 'Access-Control-Allow-Headers'],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
