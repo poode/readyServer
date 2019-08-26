7c8342e000152b9bac91490a5e1ef824fc4aebc3
poode <eng.abdul.azeem@gmail.com>
2019-08-26 14:13:25 +0200

    move comment above

--- api/router.js
+++ api/router.js
@@ -6,7 +6,8 @@ const { authRouter, authRoute } = require('./v1/login/loginRoute');
 
 const myRouter = (app) => {
   app.use(authRoute.BaseRoute, authRouter);
-  app.use(paginate.middleware(10, 50)); // here we can add any middelware as needed like paginate
+  // here we can add any middelware as needed like paginate.middleware
+  app.use(paginate.middleware(10, 50));
   app.use(userRoute.BaseRoute, userRouter);
 };
 
