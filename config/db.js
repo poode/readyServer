4dce84d458ca72aed8557fb3398aaa87cd27494c
Abdulazeem M. M <eng.abdul.azeem@gmail.com>
2022-02-02 18:29:22 +0200

    Fixing Dependencies

--- config/db.js
+++ config/db.js
@@ -6,18 +6,18 @@ const mongoose = require('mongoose');
 const { logger } = require('./logger');
 
 
-const URL = `${process.env.DB_TYPE}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}${process.env.CUSTOM_DB_URL}`;
+const URL = process.env.CUSTOM_DB_URL ? process.env.CUSTOM_DB_URL : `${process.env.DB_TYPE}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-mongoose.connect(URL, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
-const db = mongoose.connection;
+mongoose.connect(URL)
+    .then(() => {
+      logger.info('Connected to Database...')
+    })
+    .catch(e => {
+      logger.error('Unable to connect to Database...', e)
+      process.exit(0);
+    });
 
-db.on('error', () => logger.info('Unable to connect to Database...'));
-
-db.on('open', () => logger.info('Connected to Database...'));
-
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
 if (process.env.NODE_ENV === 'dev') mongoose.set('debug', true);
 
 module.exports = {
